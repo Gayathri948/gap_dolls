125e13977fe86c4a086f809681d195c06b58bdb5
Gayathri Eluri <W0848680@myscc.ca>
2024-03-16 22:54:37 -0400

    appsettings

---
+++ gapdolls/dbo.__EFMigrationsHistory.data.sql
@@ -0,0 +1,7 @@
+INSERT INTO [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'00000000000000_CreateIdentitySchema', N'8.0.3')
+INSERT INTO [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20240315203104_InitialCreate', N'8.0.3')
+INSERT INTO [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20240315210345_CreateCustomer', N'8.0.3')
+INSERT INTO [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20240315210937_CreateIdentity', N'8.0.3')
+INSERT INTO [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20240315211118_CreateCustomer', N'8.0.3')
+INSERT INTO [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20240315211530_CreateCustomer', N'8.0.3')
+INSERT INTO [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20240315212037_CreateCustomer.Designer', N'8.0.3')
